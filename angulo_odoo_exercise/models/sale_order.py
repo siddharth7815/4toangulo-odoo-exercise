99f180a79d91369bfa974db21824564cc1b127ec
Siddharth Jambukiya <siddharth.jambukiya@gmail.com>
2020-02-19 18:20:58 +0530

    [fix issue]

--- angulo_odoo_exercise/models/sale_order.py
+++ angulo_odoo_exercise/models/sale_order.py
@@ -9,7 +9,7 @@ class SaleOrder(models.Model):
     def get_orderlines_for_report(self):
         line_dic = {}
         for line in self.order_line:
-            if line.product_id.categ_id.name in line_dic:
+            if line.product_id.categ_id in line_dic:
                 line_dic[line.product_id.categ_id].append(line)
             else:
                 line_dic[line.product_id.categ_id] = [line]
